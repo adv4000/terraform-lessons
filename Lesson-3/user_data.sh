75eb4ee34165e2ed5a54db52720cbefa5e44cf65
Denis Astahov <advit.devops@gmail.com>
2019-06-06 16:05:05 -0700

    Lesson with user_data in external file

---
+++ Lesson-3/user_data.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform with external script!"  >  /var/www/html/index.html
+echo "New File in UserData" >> /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
