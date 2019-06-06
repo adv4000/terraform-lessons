6c55461fe5c897b9c80edec0c2c257d5eaa79002
Denis Astahov <advit.devops@gmail.com>
2019-06-06 15:28:50 -0700

    Lesson with Bootstrap One WebServer

---
+++ Lesson-2/WebServer.tf
@@ -0,0 +1,65 @@
+#----------------------------------------------------
+# My Terraform
+#
+# Build WebServer during Bootrsap protected_settings
+#
+# Made by Denis Astahov
+#-----------------------------------------------------
+
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+
+resource "aws_instance" "my_webserver" {
+  ami                    = "ami-03a71cec707bfc3d7"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.my_webserver.id]
+  user_data              = <<EOF
+#!/bin/bash
+yum -y update
+yum -y install httpd
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform!"  >  /var/www/html/index.html
+sudo service httpd start
+chkconfig httpd on
+EOF
+
+  tags = {
+    Name = "Web Server build by Terraform"
+  }
+}
+
+
+resource "aws_security_group" "my_webserver" {
+  name = "WebServer Security Group"
+  description = "My First SecurityGroup"
+
+  ingress {
+    description = "Allow HTTP access from any address in Internet"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow HTTPS access from any address in Internet"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "WebServer Security Group"
+  }
+}
