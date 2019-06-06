1e013d4e4faed6ea24e4e139523c4b1876d20100
Denis Astahov <advit.devops@gmail.com>
2019-06-05 18:21:00 -0700

    Lesson Create,Update,Destroy

--- Lesson-1/Lesson-1.tf
+++ Lesson-1/Lesson-1.tf
@@ -1,6 +1,24 @@
 provider "aws" {}
 
-resource "aws_instance" "my_first_ec2_instance" {
-  ami           = "ami-07ab3281411d31d04"
-  instance_type = "t2.micro"
+
+resource "aws_instance" "my_Ubuntu" {
+  ami           = "ami-090f10efc254eaf55"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name    = "My Ubuntu Server"
+    Owner   = "Denis Astahov"
+    Project = "Terraform Lessons"
+  }
+}
+
+resource "aws_instance" "my_Amazon" {
+  ami           = "ami-03a71cec707bfc3d7"
+  instance_type = "t3.small"
+
+  tags = {
+    Name    = "My Amazon Server"
+    Owner   = "Denis Astahov"
+    Project = "Terraform Lessons"
+  }
 }
