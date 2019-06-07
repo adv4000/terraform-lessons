ed00ea139ea19a489141d2b5e5db5ebcddf0dc61
Denis Astahov <advit.devops@gmail.com>
2019-06-07 02:08:54 -0700

    Lesson with WebServer via bootstrap

--- Lesson-2/WebServer.tf
+++ Lesson-2/WebServer.tf
@@ -1,10 +1,10 @@
-#----------------------------------------------------
+#----------------------------------------------------------
 # My Terraform
 #
-# Build WebServer during Bootrsap protected_settings
+# Build WebServer during Bootstrap
 #
 # Made by Denis Astahov
-#-----------------------------------------------------
+#----------------------------------------------------------
 
 
 provider "aws" {
@@ -27,7 +27,8 @@ chkconfig httpd on
 EOF
 
   tags = {
-    Name = "Web Server build by Terraform"
+    Name = "Web Server Build by Terraform"
+    Owner = "Denis Astahov"
   }
 }
 
@@ -37,7 +38,6 @@ resource "aws_security_group" "my_webserver" {
   description = "My First SecurityGroup"
 
   ingress {
-    description = "Allow HTTP access from any address in Internet"
     from_port = 80
     to_port = 80
     protocol = "tcp"
@@ -45,7 +45,6 @@ resource "aws_security_group" "my_webserver" {
   }
 
   ingress {
-    description = "Allow HTTPS access from any address in Internet"
     from_port = 443
     to_port = 443
     protocol = "tcp"
@@ -60,6 +59,7 @@ resource "aws_security_group" "my_webserver" {
   }
 
   tags = {
-    Name = "WebServer Security Group"
+    Name = "Web Server SecurityGroup"
+    Owner = "Denis Astahov"
   }
 }
