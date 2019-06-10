c056b8183491b423a69af3c6a7e045d41e0daa50
Denis Astahov <advit.devops@gmail.com>
2019-06-09 21:01:47 -0700

    Lesson with AWS Data Sources

--- Lesson-9/main.tf
+++ Lesson-9/main.tf
@@ -1,71 +1,69 @@
-#----------------------------------------------------------
-# My Terraform
-#
-# Copyleft(c) by Denis Astahov
-#----------------------------------------------------------
-
 provider "aws" {}
 
 
 data "aws_availability_zones" "working" {}
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
+data "aws_vpcs" "my_vpcs" {}
 
 
-data "aws_vpc" "my_prod_vpc" {
+data "aws_vpc" "prod_vpc" {
   tags = {
     Name = "prod"
   }
 }
 
 
-
-resource "aws_subnet" "subnet1" {
-
-  vpc_id            = data.aws_vpc.my_prod_vpc.id
+resource "aws_subnet" "prod_subnet_1" {
+  vpc_id            = data.aws_vpc.prod_vpc.id
   availability_zone = data.aws_availability_zones.working.names[0]
   cidr_block        = "10.10.1.0/24"
   tags = {
     Name    = "Subnet-1 in ${data.aws_availability_zones.working.names[0]}"
-    Account = data.aws_caller_identity.current.account_id
+    Account = "Subnet in Account ${data.aws_caller_identity.current.account_id}"
+    Region  = data.aws_region.current.description
   }
 }
 
-resource "aws_subnet" "subnet2" {
-
-  vpc_id            = data.aws_vpc.my_prod_vpc.id
+resource "aws_subnet" "prod_subnet_2" {
+  vpc_id            = data.aws_vpc.prod_vpc.id
   availability_zone = data.aws_availability_zones.working.names[1]
   cidr_block        = "10.10.2.0/24"
   tags = {
     Name    = "Subnet-2 in ${data.aws_availability_zones.working.names[1]}"
-    Account = data.aws_caller_identity.current.account_id
+    Account = "Subnet in Account ${data.aws_caller_identity.current.account_id}"
+    Region  = data.aws_region.current.description
   }
 }
 
 
 
-
-
-output "aws_account_id" {
-  value = data.aws_caller_identity.current.account_id
-}
-
-output "aws_availability_zones" {
-  value = data.aws_availability_zones.working.names
-}
-
-output "aws_region_name" {
-  value = data.aws_region.current.name
-}
-
-output "aws_region_description" {
-  value = data.aws_region.current.description
-}
-
 output "prod_vpc_id" {
-  value = data.aws_vpc.my_prod_vpc.id
+  value = data.aws_vpc.prod_vpc.id
 }
 
 output "prod_vpc_cidr" {
-  value = data.aws_vpc.my_prod_vpc.cidr_block
+  value = data.aws_vpc.prod_vpc.cidr_block
+}
+
+output "aws_vpcs" {
+  value = data.aws_vpcs.my_vpcs.ids
+}
+
+
+output "data_aws_availability_zones" {
+  value = data.aws_availability_zones.working.names
+}
+
+
+output "data_aws_caller_identity" {
+  value = data.aws_caller_identity.current.account_id
+}
+
+output "data_aws_region_name" {
+  value = data.aws_region.current.name
+}
+
+output "data_aws_region_description" {
+  value = data.aws_region.current.description
 }
