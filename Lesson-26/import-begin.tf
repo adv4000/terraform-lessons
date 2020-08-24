5317eb21b4ba21ef3f58aaba4c2014a7a4a2981d
Denis Astahov <denis@opsguru.io>
2020-08-24 16:20:41 -0700

    Lesson with Terraform Import existing resources

---
+++ Lesson-26/import-begin.tf
@@ -0,0 +1,20 @@
+# terraform import aws_instance.node1 i-0417da3dfcfd6e059
+# terraform import aws_instance.node2 i-0b92baf1fa014b3e2
+# terraform import aws_instance.node3 i-0ca6e4b3d52437673
+# terraform import aws_security_group.nomad sg-0bb76870a0cbc887a
+
+resource "aws_instance" "node1" {
+
+}
+
+resource "aws_instance" "node2" {
+
+}
+
+resource "aws_instance" "node3" {
+
+}
+
+resource "aws_security_group" "nomad" {
+
+}
--- Lesson-26/import-finish.tf
+++ Lesson-26/import-finish.tf
@@ -1,8 +1,3 @@
-# terraform import aws_instance.node1 i-0417da3dfcfd6e059
-# terraform import aws_instance.node2 i-0b92baf1fa014b3e2
-# terraform import aws_instance.node3 i-0ca6e4b3d52437673
-# terraform import aws_security_group.nomad sg-0bb76870a0cbc887a
-
 resource "aws_instance" "node1" {
   ami                    = "ami-0a634ae95e11c6f91"
   instance_type          = "t3.micro"
