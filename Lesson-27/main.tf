ceb8e86575b8cbc07b1485e054d5697d2429220c
Denis Astahov <denis@opsguru.io>
2020-08-25 20:45:44 -0700

    Lesson with Terraform Taint to Recreate Resources

---
+++ Lesson-27/main.tf
@@ -0,0 +1,33 @@
+# terraform taint aws_instance.node2
+#
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "node1" {
+  ami           = "ami-05655c267c89566dd"
+  instance_type = "t3.micro"
+  tags = {
+    Name  = "Node-1"
+    Owner = "Denis Astahov"
+  }
+}
+
+resource "aws_instance" "node2" {
+  ami           = "ami-05655c267c89566dd"
+  instance_type = "t3.micro"
+  tags = {
+    Name  = "Node-2"
+    Owner = "Denis Astahov"
+  }
+}
+
+resource "aws_instance" "node3" {
+  ami           = "ami-05655c267c89566dd"
+  instance_type = "t3.micro"
+  tags = {
+    Name  = "Node-3"
+    Owner = "Denis Astahov"
+  }
+  depends_on = [aws_instance.node2]
+}
