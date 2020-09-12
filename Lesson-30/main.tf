4b2002ef3ba85894254a83178a7a1718eda92212
Denis Astahov <denis@opsguru.io>
2020-09-12 16:42:15 -0700

    Lesson about Terraform Cloud

--- Lesson-30/main.tf
+++ Lesson-30/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "web" {
 yum -y update
 yum -y install httpd
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h2>PROD WebServer with IP: $myip</h2><br>Build by Terraform!"  >  /var/www/html/index.html
+echo "<h2>${var.server_name}-WebServer with IP: $myip</h2><br>Build by Terraform!"  >  /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
 EOF
