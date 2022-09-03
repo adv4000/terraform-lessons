e20d158bebc5fb1d9c1bacd71af37062aceb4160
Denis Astahov <adv4000@gmail.com>
2022-09-03 10:15:09 -0700

    Lesson with HighlyAvailable WEB and ZeroDown Time Blue/Green Deployment

--- Lesson-11/main.tf
+++ Lesson-11/main.tf
@@ -13,6 +13,7 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
 
 data "aws_availability_zones" "available" {}
 data "aws_ami" "latest_amazon_linux" {
@@ -26,7 +27,8 @@ data "aws_ami" "latest_amazon_linux" {
 
 #--------------------------------------------------------------
 resource "aws_security_group" "web" {
-  name = "Dynamic Security Group"
+  name   = "Dynamic Security Group"
+  vpc_id = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
 
   dynamic "ingress" {
     for_each = ["80", "443"]
