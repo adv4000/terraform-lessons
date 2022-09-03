46c66fdedfd5247f5d2178a14a43718645ba712f
Denis Astahov <adv4000@gmail.com>
2022-09-03 10:14:52 -0700

    Lesson with Dependencies depends_on

--- Lesson-08/main.tf
+++ Lesson-08/main.tf
@@ -8,6 +8,8 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
+
 resource "aws_instance" "my_server_web" {
   ami                    = "ami-03a71cec707bfc3d7"
   instance_type          = "t3.micro"
@@ -45,7 +47,9 @@ resource "aws_instance" "my_server_db" {
 
 
 resource "aws_security_group" "my_webserver" {
-  name = "My Security Group"
+  name   = "My Security Group"
+  vpc_id = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
+
 
   dynamic "ingress" {
     for_each = ["80", "443", "22"]
