6cda02fc6bf853b3b7a4ac035eb9b85388f51208
Denis Astahov <adv4000@gmail.com>
2022-09-03 10:14:14 -0700

    Lesson with LifeCycle and EIP

--- Lesson-06/WebServer.tf
+++ Lesson-06/WebServer.tf
@@ -11,6 +11,7 @@ provider "aws" {
   region = "ca-central-1"
 }
 
+resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
 
 resource "aws_eip" "my_static_ip" {
   instance = aws_instance.my_webserver.id
@@ -40,14 +41,13 @@ resource "aws_instance" "my_webserver" {
   lifecycle {
     create_before_destroy = true
   }
-
 }
 
 
 resource "aws_security_group" "my_webserver" {
   name        = "WebServer Security Group"
   description = "My First SecurityGroup"
-
+  vpc_id      = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
 
   dynamic "ingress" {
     for_each = ["80", "443"]
