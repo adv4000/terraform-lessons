1e8c983ef006eef6fd8f1b8c68be627de8c7291c
Denis Astahov <adv4000@gmail.com>
2022-09-03 10:13:54 -0700

    Lesson with Dynamic Blocks in SecurityGroup

--- Lesson-05/DynamicSecurityGroup.tf
+++ Lesson-05/DynamicSecurityGroup.tf
@@ -11,9 +11,12 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
 
 resource "aws_security_group" "my_webserver" {
-  name = "Dynamic Security Group"
+  name   = "Dynamic Security Group"
+  vpc_id = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
+
 
   dynamic "ingress" {
     for_each = ["80", "443", "8080", "1541", "9092", "9093"]
