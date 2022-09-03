4dc5858c85b82c32216989ac55a383c5c48b23af
Denis Astahov <adv4000@gmail.com>
2022-09-03 10:13:07 -0700

    Lesson with WebServer via bootstrap

--- Lesson-02/WebServer.tf
+++ Lesson-02/WebServer.tf
@@ -11,6 +11,7 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+resource "aws_default_vpc" "default" {} # This need to be added since AWS Provider v4.29+ to get VPC id
 
 resource "aws_instance" "my_webserver" {
   ami                    = "ami-03a71cec707bfc3d7"
@@ -27,39 +28,40 @@ chkconfig httpd on
 EOF
 
   tags = {
-    Name = "Web Server Build by Terraform"
+    Name  = "Web Server Build by Terraform"
     Owner = "Denis Astahov"
   }
 }
 
 
 resource "aws_security_group" "my_webserver" {
-  name = "WebServer Security Group"
+  name        = "WebServer Security Group"
   description = "My First SecurityGroup"
+  vpc_id      = aws_default_vpc.default.id # This need to be added since AWS Provider v4.29+ to set VPC id
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
-    Name = "Web Server SecurityGroup"
+    Name  = "Web Server SecurityGroup"
     Owner = "Denis Astahov"
   }
 }
