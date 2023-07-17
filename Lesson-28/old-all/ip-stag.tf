4e982f90f2d722c6177a8f0bcb5707c8a0d3194f
Denis Astahov <adv4000@gmail.com>
2023-07-16 22:34:11 -0700

    Lesson with Terraform State Manipulations - Refactoring code

--- Lesson-28/old-all/ip-stag.tf
+++ Lesson-28/old-all/ip-stag.tf
@@ -1,2 +1,2 @@
-resource "aws_eip" "stag-ip1" { domain = "vpc" } # Need to add in new AWS Provider version
-resource "aws_eip" "stag-ip2" { domain = "vpc" } # Need to add in new AWS Provider version
+resource "aws_eip" "stag-ip1" { domain = "vpc" }  # Need to add in new AWS Provider version
+resource "aws_eip" "stag-ip2" { domain = "vpc" }  # Need to add in new AWS Provider version
